200f159db88e6570f02e8509ab63f590da4b377d
Thomas Helmkay <thomas.helmkay@e-healthinnovations.com>
2021-03-23 15:46:09 +0000

    Initial commit

---
+++ src/main/java/com/example/xdesign/XdesignApplication.java
@@ -0,0 +1,13 @@
+package com.example.xdesign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XdesignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XdesignApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/xdesign/XdesignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.xdesign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XdesignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
